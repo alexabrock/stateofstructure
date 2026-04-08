1ae7aa0ecca67a1b41260be065142bf77ca06c2c
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-04-08 14:12:14 +0200

    fix maven warning

--- src/main/java/com/hhu/util/datastructureView/impl/VArrayListGraphBuilder.java
+++ src/main/java/com/hhu/util/datastructureView/impl/VArrayListGraphBuilder.java
@@ -17,18 +17,18 @@ public class VArrayListGraphBuilder implements GraphBuilder {
         if (!(collection instanceof VArrayList<?>)) {
                 throw new IllegalArgumentException("Expected VLinkedList");
             }
-            VArrayList<Object> list = (VArrayList<Object>) collection;
+            VArrayList<?> list = (VArrayList<?>) collection;
 
         Graph g = graph("listGraph").directed()
                 .graphAttr().with(Rank.dir(LEFT_TO_RIGHT))
                 .nodeAttr().with(Shape.RECTANGLE);
 
         for (int i = 0; i < list.size(); i++) {
-            String nodeName = list.get(i).toString();
+            String nodeName = String.valueOf(list.get(i));
             g = g.with(node(nodeName));
 
             if (i > 0) {
-                String prevNodeName = list.get(i - 1).toString();
+                String prevNodeName = String.valueOf(list.get(i - 1));
                 // next pointer
                 g = g.with(node(prevNodeName).link(node(nodeName)));
                 // prev pointer
