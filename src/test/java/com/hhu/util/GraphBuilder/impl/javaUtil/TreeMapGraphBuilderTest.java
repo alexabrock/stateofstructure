7c4e7d39e9cdad0641bc3d042375833ab4620603
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-04-09 19:29:58 +0200

    Fix TreemapTests

--- src/test/java/com/hhu/util/GraphBuilder/impl/javaUtil/TreeMapGraphBuilderTest.java
+++ src/test/java/com/hhu/util/GraphBuilder/impl/javaUtil/TreeMapGraphBuilderTest.java
@@ -1,5 +1,6 @@
 package com.hhu.util.GraphBuilder.impl.javaUtil;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.TreeMap;
@@ -20,62 +21,59 @@ public class TreeMapGraphBuilderTest {
         tree = new TreeMap<>();
     }
 
-    private boolean hasEdge(MutableGraph graph, String from, String to) {
-        return graph.nodes().stream()
-                .filter(n -> n.name().toString().equals(from))
-                .findFirst()
-                .orElseThrow()
-                .links().stream()
-                .anyMatch(link -> link.to().name().toString().equals(to));
+    private MutableGraph createMutableGraph() {
+        return (MutableGraph) GraphBuilderFactory.getBuilder(tree).buildGraph(tree);
     }
 
     @Test
     public void graphContainsSingleStringElement() {
-        tree.put("a",1);
+        tree.put("a", 1);
 
-        MutableGraph graph = (MutableGraph) GraphBuilderFactory
-                .getBuilder(tree)
-                .buildGraph(tree);
+        MutableGraph graph = createMutableGraph();
 
         assertTrue(
                 graph.nodes().stream()
-                        .anyMatch(n -> n.name().toString().equals("a")));
+                        .anyMatch(n -> n.name().toString().equals("a, 1")));
     }
 
     @Test
     public void graphContainsMultipleStringElements() {
-        tree.put("Lotte",  1);
-        tree.put("Dieter",2);
-        tree.put("Alexa",3);
-        tree.put("Holland",4);
 
-        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(tree).buildGraph(tree);
+        tree.put("Lotte", 1);
+        tree.put("Dieter", 2);
+        tree.put("Alexa", 3);
+        tree.put("Holland", 4);
+        
+        MutableGraph graph = createMutableGraph();
 
-        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Lotte")));
-        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Dieter")));
-        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Alexa")));
-        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Holland")));
+        assertTrue(graph.nodes().stream()
+                .anyMatch(n -> n.name().toString().equals("Lotte, 1")));
+
+        assertTrue(graph.nodes().stream()
+                .anyMatch(n -> n.name().toString().equals("Dieter, 2")));
+
+        assertTrue(graph.nodes().stream()
+                .anyMatch(n -> n.name().toString().equals("Alexa, 3")));
+
+        assertTrue(graph.nodes().stream()
+                .anyMatch(n -> n.name().toString().equals("Holland, 4")));
     }
 
     @Test
     public void graphEmpty() {
 
-        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(tree).buildGraph(tree);
+        MutableGraph graph = createMutableGraph();
 
         assertTrue(graph.nodes().isEmpty());
     }
 
     @Test
-    public void graphHasCorrectEdge() {
-        tree.put("Lotte", 1);
-        tree.put("Dieter", 2);
-        tree.put("Alexa", 3);
-        tree.put("Holland", 4);
+    public void graphWithOneNodeHasNoLinks() {
+        tree.put("Alexa", 1);
 
-        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(tree).buildGraph(tree);
+        MutableGraph graph = createMutableGraph();
 
-        assertTrue(hasEdge(graph, "Alexa", "3"));
-        
+        assertTrue(graph.links().isEmpty());
     }
 
 
