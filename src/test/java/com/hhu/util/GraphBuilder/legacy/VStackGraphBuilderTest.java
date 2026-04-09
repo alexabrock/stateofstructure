628ce64efe63c62ab4e419be7e06d894de1c7a95
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-04-09 16:58:40 +0200

    Disable V Datastructure Tests

---
+++ src/test/java/com/hhu/util/GraphBuilder/legacy/VStackGraphBuilderTest.java
@@ -0,0 +1,57 @@
+package com.hhu.util.GraphBuilder.legacy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.hhu.legacyDatastructures.VStack;
+import com.hhu.legacyDatastructures.VTreeMap;
+import com.hhu.util.datastructureView.GraphBuilderFactory;
+
+//Mutable graph, weil man so schön an die einzelnen Nodes zum testen kommt
+import guru.nidi.graphviz.model.MutableGraph;
+import org.junit.Ignore;
+
+@Ignore
+public class VStackGraphBuilderTest {
+
+    @Test
+    public void graphContainsSingleStringElement() {
+        VStack<String> stack = new VStack<>();
+        stack.add("a");
+
+        MutableGraph graph = (MutableGraph) GraphBuilderFactory
+                .getBuilder(stack)
+                .buildGraph(stack);
+
+        assertTrue(
+                graph.nodes().stream()
+                        .anyMatch(n -> n.name().toString().equals("a")));
+    }
+
+    @Test
+    public void graphContainsMultipleStringElements() {
+        VStack<String> stack = new VStack<>();
+        stack.add("Lotte");
+        stack.add("Dieter");
+        stack.add("Alexa");
+        stack.add("Holland");
+
+        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(stack).buildGraph(stack);
+
+        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Lotte")));
+        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Dieter")));
+        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Alexa")));
+        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Holland")));
+    }
+
+    @Test
+    public void graphEmpty() {
+        VStack<String> stack = new VStack<>();
+
+        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(stack).buildGraph(stack);
+
+        assertTrue(graph.nodes().isEmpty());
+    }
+    
+}
---
+++ src/test/java/com/hhu/util/GraphBuilder/legacy/VTreeMapGraphBuilderTest.java
@@ -0,0 +1,82 @@
+package com.hhu.util.GraphBuilder.legacy;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.hhu.legacyDatastructures.VLinkedList;
+import com.hhu.legacyDatastructures.VTreeMap;
+import com.hhu.util.datastructureView.GraphBuilderFactory;
+
+//Mutable graph, weil man so schön an die einzelnen Nodes zum testen kommt
+import guru.nidi.graphviz.model.MutableGraph;
+import org.junit.Ignore;
+
+@Ignore
+public class VTreeMapGraphBuilderTest {
+
+    private boolean hasEdge(MutableGraph graph, String from, String to) {
+        return graph.nodes().stream()
+                .filter(n -> n.name().toString().equals(from))
+                .findFirst()
+                .orElseThrow()
+                .links().stream()
+                .anyMatch(link -> link.to().name().toString().equals(to));
+    }
+
+    @Test
+    public void graphContainsSingleStringElement() {
+        VTreeMap<String, Integer> tree = new VTreeMap<>();
+        tree.put("a",1);
+
+        MutableGraph graph = (MutableGraph) GraphBuilderFactory
+                .getBuilder(tree)
+                .buildGraph(tree);
+
+        assertTrue(
+                graph.nodes().stream()
+                        .anyMatch(n -> n.name().toString().equals("a")));
+    }
+
+    @Test
+    public void graphContainsMultipleStringElements() {
+        VTreeMap<String, Integer> tree = new VTreeMap<>();
+        tree.put("Lotte",  1);
+        tree.put("Dieter",2);
+        tree.put("Alexa",3);
+        tree.put("Holland",4);
+
+        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(tree).buildGraph(tree);
+
+        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Lotte")));
+        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Dieter")));
+        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Alexa")));
+        assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Holland")));
+    }
+
+    @Test
+    public void graphEmpty() {
+        VTreeMap<String, Integer> tree = new VTreeMap<>();
+
+        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(tree).buildGraph(tree);
+
+        assertTrue(graph.nodes().isEmpty());
+    }
+
+    @Test
+    public void graphHasCorrectEdge() {
+        VTreeMap<String, Integer> tree = new VTreeMap<>();
+        tree.put("Lotte", 1);
+        tree.put("Dieter", 2);
+        tree.put("Alexa", 3);
+        tree.put("Holland", 4);
+
+        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(tree).buildGraph(tree);
+
+        assertTrue(hasEdge(graph, "Alexa", "3"));
+        
+    }
+
+
+}
