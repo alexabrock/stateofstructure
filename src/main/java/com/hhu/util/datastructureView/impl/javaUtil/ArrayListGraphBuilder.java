dff022573f9c9f84fbd62206ed8882e1880fb670
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-04-09 16:57:16 +0200

    add comment

--- src/main/java/com/hhu/util/datastructureView/impl/javaUtil/ArrayListGraphBuilder.java
+++ src/main/java/com/hhu/util/datastructureView/impl/javaUtil/ArrayListGraphBuilder.java
@@ -31,6 +31,7 @@ public class ArrayListGraphBuilder implements GraphBuilder {
 
         Graph g = graph("listGraph").directed()
                 .graphAttr().with(
+                        //damit die einzelnen Nodes von rechts nach links angezeigt werden
                         Rank.dir(TOP_TO_BOTTOM),
                         Attributes.attr("nodesep", "0"), // horizontal spacing
                         Attributes.attr("ranksep", "0") // vertical spacing
