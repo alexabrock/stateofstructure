d39e956301466bd408b1665782ad0cb9cdacbb6d
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-04-09 18:24:00 +0200

    split graphviz app

--- src/main/java/GraphvizApp.java
+++ src/main/java/GraphvizApp.java
@@ -12,20 +12,14 @@ import com.hhu.util.DrawCalls;
 import com.hhu.views.application.Application;
 
 public class GraphvizApp {
+    
     public static void main(String[] args) {
-        //startVListApplication();
 
-        //startVStackApplication();
-
-        //startVTreeMapApplication();
-
-        //startVArrayListApplication();
-
-        startStackApplicationWithFunnyDrawCalls();
+        //startStackApplicationWithFunnyDrawCalls();
 
         //startArrayListApplicationWithFunnyDrawCalls();
 
-       // startTreeMapApplicationWithFunnyDrawCalls();
+        startTreeMapApplicationWithFunnyDrawCalls();
 
         //startLinkedHashSetApplicationWithFunnyDrawCalls();
 
@@ -110,47 +104,4 @@ public class GraphvizApp {
 
     }
 
-    private static void startVArrayListApplication() {
-        VArrayList<Integer> list = new VArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-       // Application.startApplication(list);
-    }
-
-    static void startVListApplication() {
-
-        VLinkedList<String> list = new VLinkedList<>();
-        list.add("Hello");
-        list.add("Progra");
-        list.add("2026");
-
-        //Application.startApplication( list);
-    }
-
-    static void startVStackApplication() {
-
-
-        VStack<String> stack = new VStack<>();
-        stack.push("Hello");
-        stack.push("Progra");
-        stack.push("middleElement");
-        stack.push("2026");
-        stack.pop();
-        stack.search("Hello");
-
-        //Application.startApplication( stack);
-    }
-
-    static void startVTreeMapApplication() {
-
-
-        VTreeMap<String, Integer> map = new VTreeMap<>();
-        map.put("hello", 1);
-        map.put("world", 2);
-        map.put("java", 3);
-
-       // Application.startApplication( map);
-    }
 }
---
+++ src/main/java/GraphvizAppOld.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import com.hhu.legacyDatastructures.VArrayList;
+import com.hhu.legacyDatastructures.VLinkedList;
+import com.hhu.legacyDatastructures.VStack;
+import com.hhu.legacyDatastructures.VTreeMap;
+import com.hhu.util.DrawCalls;
+import com.hhu.views.application.Application;
+
+public class GraphvizAppOld {
+    public static void main(String[] args) {
+        //startVListApplication();
+
+        //startVStackApplication();
+
+        //startVTreeMapApplication();
+
+        //startVArrayListApplication();
+
+    }
+
+
+    private static void startVArrayListApplication() {
+        VArrayList<Integer> list = new VArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+       // Application.startApplication(list);
+    }
+
+    static void startVListApplication() {
+
+        VLinkedList<String> list = new VLinkedList<>();
+        list.add("Hello");
+        list.add("Progra");
+        list.add("2026");
+
+        //Application.startApplication( list);
+    }
+
+    static void startVStackApplication() {
+
+
+        VStack<String> stack = new VStack<>();
+        stack.push("Hello");
+        stack.push("Progra");
+        stack.push("middleElement");
+        stack.push("2026");
+        stack.pop();
+        stack.search("Hello");
+
+        //Application.startApplication( stack);
+    }
+
+    static void startVTreeMapApplication() {
+
+
+        VTreeMap<String, Integer> map = new VTreeMap<>();
+        map.put("hello", 1);
+        map.put("world", 2);
+        map.put("java", 3);
+
+       // Application.startApplication( map);
+    }
+}
