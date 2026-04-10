b6e1c5131d726d32c752dbe08d8e3439e8927e1c
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-04-10 17:19:38 +0200

    rename test class and add tests

--- src/test/java/com/hhu/util/GraphBuilder/impl/javaUtil/LinkedListGraphBuilderTest.java
+++ src/test/java/com/hhu/util/GraphBuilder/impl/javaUtil/LinkedListGraphBuilderTest.java
@@ -1,5 +1,6 @@
 package com.hhu.util.GraphBuilder.impl.javaUtil;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.LinkedList;
@@ -8,11 +9,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.hhu.datastructureView.GraphBuilderFactory;
+import com.hhu.util.DrawCalls;
+import com.hhu.views.application.Application;
 
 //Mutable graph, weil man so schön an die einzelnen Nodes zum testen kommt
 import guru.nidi.graphviz.model.MutableGraph;
 
-public class ListGraphBuilderTest {
+public class LinkedListGraphBuilderTest {
 
     private LinkedList<String> list;
 
@@ -21,19 +24,21 @@ public class ListGraphBuilderTest {
         list = new LinkedList<>();
     }
 
+    private MutableGraph createMutableGraph() {
+        return (MutableGraph) GraphBuilderFactory.getBuilder(list).buildGraph(list);
+    }
+
     @Test
     public void graphContainsSingleStringElement() {
         list.add("a");
 
-        MutableGraph graph = (MutableGraph) GraphBuilderFactory
-                .getBuilder(list)
-                .buildGraph(list);
+        MutableGraph graph = createMutableGraph();
 
         assertTrue(
                 graph.nodes().stream()
                         .anyMatch(n -> n.name().toString().equals("a")));
     }
-    
+
     @Test
     public void graphContainsMultipleStringElements() {
         list.add("Lotte");
@@ -41,8 +46,7 @@ public class ListGraphBuilderTest {
         list.add("Alexa");
         list.add("Holland");
 
-        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(list).buildGraph(list);
-
+        MutableGraph graph = createMutableGraph();
         assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Lotte")));
         assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Dieter")));
         assertTrue(graph.nodes().stream().anyMatch(n -> n.name().toString().equals("Alexa")));
@@ -52,9 +56,21 @@ public class ListGraphBuilderTest {
     @Test
     public void graphEmpty() {
 
-        MutableGraph graph = (MutableGraph) GraphBuilderFactory.getBuilder(list).buildGraph(list);
+        MutableGraph graph = createMutableGraph();
 
         assertTrue(graph.nodes().isEmpty());
     }
-}
 
+    @Test
+    public void correctAmountOfNodes() {
+        list.add("Lotte");
+        list.add("Dieter");
+        list.add("Alexa");
+        list.add("Holland");
+        
+        MutableGraph graph = createMutableGraph();
+
+        //all nodes come with 2 pointers (next & prev), except for the first and last node, which have one pointer each
+         assertEquals(list.size() *3-2, graph.nodes().size());
+    }
+}
