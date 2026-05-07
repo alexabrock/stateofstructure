f5d06a3151364d729a4d5e5bd4f296be4a2caf82
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-07 21:05:56 +0200

    add stacktrace printing

--- src/main/java/com/hhu/util/compiler/CompilationException.java
+++ src/main/java/com/hhu/util/compiler/CompilationException.java
@@ -3,5 +3,6 @@ package com.hhu.util.compiler;
 public class CompilationException extends RuntimeException {
     CompilationException(String msg , Exception e) {
         super(msg, e);
+        e.printStackTrace();
     }
 }
