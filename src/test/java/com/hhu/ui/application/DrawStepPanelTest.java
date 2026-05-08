718c15f69ef8effbb9f1c81539bf47bc0cb7d976
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-08 23:03:58 +0200

    Add DrawStepPanelTest

---
+++ src/test/java/com/hhu/ui/application/DrawStepPanelTest.java
@@ -0,0 +1,46 @@
+package com.hhu.ui.application;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.junit.Test;
+
+import com.hhu.util.DrawStep;
+
+public class DrawStepPanelTest {
+
+    @Test
+    public void createVisualizationPanelReturnsJPanel() {
+        CodePanelScroller codePanelScroller = mock(CodePanelScroller.class);
+        DrawStepPanel drawStepPanel = new DrawStepPanel(codePanelScroller);
+        JPanel panel = new JPanel();
+
+        JPanel jPanel = drawStepPanel.createVisualizationPanel(panel, panel);
+
+        assertNotNull(jPanel);
+        assertTrue(jPanel instanceof JPanel);
+    }
+
+    
+    @Test
+    public void testCreateCenterPanelReturnsAJPanel() {
+        CodePanelScroller codePanelScroller = mock(CodePanelScroller.class);
+        DrawStepPanel drawStepPanel = new DrawStepPanel(codePanelScroller);
+        DrawStep drawStep = mock(DrawStep.class);
+        
+        when(drawStep.codePanel()).thenReturn(new JPanel());
+        when(drawStep.memory()).thenReturn(new JPanel());
+        when(drawStep.datastructure()).thenReturn(new JPanel());
+        when(drawStep.name()).thenReturn(new JLabel("test"));
+
+        JPanel jPanel = drawStepPanel.createCenterPanel(drawStep);
+
+        assertNotNull(jPanel);
+        assertTrue(jPanel instanceof JPanel);
+    }
+}
