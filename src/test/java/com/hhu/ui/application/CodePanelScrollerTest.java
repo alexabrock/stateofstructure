cffc351c3f24aed0e1945cbda287b923a290fa17
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-08 22:41:37 +0200

    AddCodePanelScrollerTest

---
+++ src/test/java/com/hhu/ui/application/CodePanelScrollerTest.java
@@ -0,0 +1,48 @@
+package com.hhu.ui.application;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.swing.JPanel;
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.junit.Test;
+
+public class CodePanelScrollerTest {
+    @Test
+    public void readCodeWorksWhenRootIsRSyntaxTextArea() {
+        RSyntaxTextArea root = new RSyntaxTextArea();
+        root.setText("Hello");
+        CodePanelScroller codePanelScroller = new CodePanelScroller();
+
+        String result = codePanelScroller.readCode(root);
+
+        assertEquals("Hello", result);
+    }
+    
+    @Test
+    public void readCodeWorksWhenRootIsInComponent() {
+        RSyntaxTextArea textArea = new RSyntaxTextArea();
+        JPanel root = new JPanel();
+        root.add(textArea);
+        textArea.setText("Hello");
+        CodePanelScroller codePanelScroller = new CodePanelScroller();
+
+        String result = codePanelScroller.readCode(root);
+
+        assertEquals("Hello", result);
+    }
+
+    @Test
+    public void findsSyntaxTextArea() {
+        RSyntaxTextArea textArea = new RSyntaxTextArea();
+        JPanel root = new JPanel();
+        root.add(textArea);
+        CodePanelScroller codePanelScroller = new CodePanelScroller();
+
+        RSyntaxTextArea result = codePanelScroller.findSyntaxTextArea(root);
+
+        assertEquals(textArea, result);
+    }
+
+    
+}
