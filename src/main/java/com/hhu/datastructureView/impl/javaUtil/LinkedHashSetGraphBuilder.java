f0cce8b1ca75272e5059f76c9c98a6ee1fdc6f01
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-11 23:47:42 +0200

    change  linkedHashSet GraphBuilder

--- src/main/java/com/hhu/datastructureView/impl/javaUtil/LinkedHashSetGraphBuilder.java
+++ src/main/java/com/hhu/datastructureView/impl/javaUtil/LinkedHashSetGraphBuilder.java
@@ -1,5 +1,6 @@
 package com.hhu.datastructureView.impl.javaUtil;
 
+import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
 import static guru.nidi.graphviz.attribute.Rank.RankDir.TOP_TO_BOTTOM;
 import static guru.nidi.graphviz.model.Factory.graph;
 import static guru.nidi.graphviz.model.Factory.node;
@@ -11,59 +12,66 @@ import guru.nidi.graphviz.attribute.Arrow;
 import guru.nidi.graphviz.attribute.Rank;
 import guru.nidi.graphviz.attribute.Rank.RankType;
 import guru.nidi.graphviz.attribute.Shape;
+import guru.nidi.graphviz.attribute.Style;
 import guru.nidi.graphviz.model.Graph;
-import guru.nidi.graphviz.model.Node;
-/* Builds a GraphViz Graph for LinkedHashSets */
-public class LinkedHashSetGraphBuilder  {
 
-//Not using NodeBuilder, since Nodes shouldn't enter the HashSet twice.
+import static guru.nidi.graphviz.attribute.Attributes.attr;
+import guru.nidi.graphviz.model.Node;
+
+/* Builds a GraphViz Graph for LinkedHashSets */
+public class LinkedHashSetGraphBuilder {
+
+    // Not using NodeBuilder, since Nodes shouldn't enter the HashSet twice.
     public static Graph buildGraph(LinkedHashSet<?> set) {
-        
+
         Graph g = graph("linkedHashSetGraph").directed()
-                .graphAttr().with(Rank.dir(TOP_TO_BOTTOM))
+                .graphAttr().with(Rank.dir(LEFT_TO_RIGHT))
                 .nodeAttr().with(Shape.RECTANGLE);
 
         // Graph with just the elements of the HashSet, to get them to be next to each
         // other
-        Graph elements = graph("elements").directed()
+        Graph elements = graph("cluster_elements").directed()
                 .graphAttr().with(
+                        attr("color", "white"),
+                        attr("penwidth", "2"),
                         Rank.inSubgraph(RankType.SAME))
                 .nodeAttr().with(Shape.RECTANGLE);
 
         // Not visualizing the HashTable Slots in the DataStructure View, since that
         // info is normally hidden from the user
-        Node hashTable = node("Hash Table").with(Shape.BOX_3D);
 
         // Subgraph for just the hashTable Node, to set it above all the other Nodes
-        g = g.with(
-                graph().graphAttr()
-                        .with(Rank.inSubgraph(RankType.SOURCE))
-                        .with(hashTable)
-            );
 
         Object prev = null;
 
         for (Object current : set) {
             Node currentNode = node(current.toString());
-            //Verweis auf HashTable
-            g = g.with(currentNode.link(to(hashTable).with(Arrow.NONE)));
-
-            if (prev != null) {
+            if (prev == null) {
+                elements = elements.with(
+                        node("head").link(
+                                to(currentNode)));
+            } else {
                 String prevNodeName = prev.toString();
                 Node prevNode = node(prevNodeName);
 
                 // before pointer
                 elements = elements.with(
                         prevNode.link(
-                                to(currentNode).with(Arrow.VEE)));
+                                to(currentNode)));
                 // after pointer
                 elements = elements.with(
                         currentNode.link(
-                                to(prevNode).with(Arrow.VEE)));
+                                to(prevNode)));
             }
-
             prev = current;
         }
+        // After the loop, 'prev' is the last element in the set
+        if (prev != null) {
+            elements = elements.with(
+                    node("tail").link(
+                            to(node(prev.toString()))));
+        }
+        
         g = g.with(elements);
         return g;
     }
