bad47526f69532dd719371abbef957d0eea5661a
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-11 23:17:39 +0200

    change stack vizualisation

--- src/main/java/com/hhu/datastructureView/impl/javaUtil/StackGraphBuilder.java
+++ src/main/java/com/hhu/datastructureView/impl/javaUtil/StackGraphBuilder.java
@@ -1,6 +1,8 @@
 package com.hhu.datastructureView.impl.javaUtil;
 
 import static com.hhu.datastructureView.NodeBuilder.getNode;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.BOTTOM_TO_TOP;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
 import static guru.nidi.graphviz.attribute.Rank.RankDir.TOP_TO_BOTTOM;
 import static guru.nidi.graphviz.model.Factory.graph;
 
@@ -10,8 +12,16 @@ import java.util.Stack;
 import guru.nidi.graphviz.attribute.Rank;
 import guru.nidi.graphviz.attribute.Rank.RankType;
 import guru.nidi.graphviz.attribute.Shape;
+import guru.nidi.graphviz.attribute.Style;
+import guru.nidi.graphviz.model.Graph;
+import guru.nidi.graphviz.model.Link;
+import guru.nidi.graphviz.model.LinkSource;
+import guru.nidi.graphviz.attribute.Attributes;
+import guru.nidi.graphviz.attribute.Rank;
+import guru.nidi.graphviz.attribute.Shape;
 import guru.nidi.graphviz.model.Graph;
 import guru.nidi.graphviz.model.Node;
+import static guru.nidi.graphviz.model.Factory.to;
 
 /* Builds a GraphViz Graph for Stacks */
 public class StackGraphBuilder {
@@ -30,26 +40,32 @@ public class StackGraphBuilder {
         // rank=same subgraph for top pointer & top element linkage
         Graph topRank = graph("topRank")
                 .graphAttr().with(Rank.inSubgraph(RankType.SAME))
-                .with(topPointer, nodes.get(0));
+                .with(topPointer, nodes.get(stack.size() - 1));
                 
         // rank=same subgraph for bottom pointer & bottom element linkage
         Graph bottomRank = graph("bottomRank")
                 .graphAttr().with(Rank.inSubgraph(RankType.SAME))
-                .with(bottomPointer, nodes.get(stack.size() - 1));
+                .with(bottomPointer, nodes.get(0));
 
-        Graph g = graph("stackGraph").directed()
-                .graphAttr().with(Rank.dir(TOP_TO_BOTTOM))
-                .nodeAttr().with(Shape.RECTANGLE);
+        Graph g = graph("stackGraph")
+                .graphAttr().with(Rank.dir(BOTTOM_TO_TOP))
+                .nodeAttr().with(Shape.RECTANGLE)
+                .graphAttr().with(
+                        //Attributes.attr("nodesep", "0"), // horizontal spacing
+                        Attributes.attr("ranksep", "0") // vertical spacing
+                );
 
         for (int i = 1; i < stack.size(); i++) {
-            g = g.with(nodes.get(i - 1).link(nodes.get(i)));
+            LinkSource link = nodes.get(i - 1).link(nodes.get(i))
+                    .with(Attributes.attr("style", "dashed")); //color not-top stack items in dashed line 
+            g = g.with(link);
         }
 
         g = g.with(
-                topPointer.link(nodes.get(0)),
-                bottomPointer.link(nodes.get(stack.size() - 1)),
-                topRank,
-                bottomRank);
+                bottomPointer.link(nodes.get(0)),
+                topPointer.link(nodes.get(stack.size() - 1)),
+                bottomRank,
+                topRank);
 
         return g;
     }
