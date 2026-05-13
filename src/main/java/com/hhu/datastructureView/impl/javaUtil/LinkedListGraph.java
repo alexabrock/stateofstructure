f97c8fa29ad81433c340f6d41523cce3c1044d7a
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-13 18:45:42 +0200

    add head & tail pointers

--- src/main/java/com/hhu/datastructureView/impl/javaUtil/LinkedListGraph.java
+++ src/main/java/com/hhu/datastructureView/impl/javaUtil/LinkedListGraph.java
@@ -2,7 +2,11 @@ package com.hhu.datastructureView.impl.javaUtil;
 
 import static com.hhu.datastructureView.NodeBuilder.getNode;
 import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.TOP_TO_BOTTOM;
 import static guru.nidi.graphviz.model.Factory.graph;
+import static guru.nidi.graphviz.model.Factory.node;
+import static guru.nidi.graphviz.model.Factory.nodeAttrs;
+import static guru.nidi.graphviz.model.Factory.to;
 
 import java.util.HashMap;
 import java.util.List;
@@ -10,7 +14,9 @@ import java.util.Map;
 
 import guru.nidi.graphviz.attribute.Rank;
 import guru.nidi.graphviz.attribute.Shape;
+import guru.nidi.graphviz.attribute.Style;
 import guru.nidi.graphviz.model.Graph;
+import guru.nidi.graphviz.model.Link;
 import guru.nidi.graphviz.model.Node;
 
 /* Builds a GraphViz Graph for LinkedLists*/
@@ -18,7 +24,7 @@ public class LinkedListGraph {
     public static Graph buildGraph(List<?> list) {
 
         Graph g = graph("listGraph").directed()
-                .graphAttr().with(Rank.dir(LEFT_TO_RIGHT))
+                .graphAttr().with(Rank.dir(TOP_TO_BOTTOM))
                 .nodeAttr().with(Shape.RECTANGLE);
 
         //cashing Nodes for linkage, sind ID broke normal factory pattern
@@ -28,16 +34,20 @@ public class LinkedListGraph {
             String nodeName = list.get(i).toString();
             Node currentNode = getNode(nodeName);
             nodeMap.put(i, currentNode);
-            g = g.with( currentNode);
+            g = g.with(currentNode);
 
             if (i > 0) {
-                Node prevNode = nodeMap.get(i-1);
+                Node prevNode = nodeMap.get(i - 1);
                 // next pointer
                 g = g.with(prevNode.link(currentNode));
                 // prev pointer
                 g = g.with(currentNode.link(prevNode));
             }
         }
+        Node head = node("head").with(Shape.PLAIN).link(to(nodeMap.get(0)));
+        Node tail = node("tail").with(Shape.PLAIN).link(to(nodeMap.get(list.size()-1)));
+
+        g = g.with(head, tail);
         return g;
     }
 }
