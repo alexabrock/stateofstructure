9b30ebd6655ed609da9e5f62d42efb9e044e1dbd
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-14 20:32:18 +0200

    remove imports

--- src/main/java/com/hhu/util/compiler/Compiler.java
+++ src/main/java/com/hhu/util/compiler/Compiler.java
@@ -2,7 +2,6 @@ package com.hhu.util.compiler;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.net.URLClassLoader;
