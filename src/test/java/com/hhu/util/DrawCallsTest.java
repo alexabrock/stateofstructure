7e496789540269aa570cffb44f6b56016be173c5
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-14 20:09:18 +0200

    add drawCalls Test

---
+++ src/test/java/com/hhu/util/DrawCallsTest.java
@@ -0,0 +1,45 @@
+package com.hhu.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class DrawCallsTest {
+    private DrawCalls drawCalls;
+    private ArrayList<Integer> mockCollection;
+
+    @Before
+    public void setUp() {
+        mockCollection = new ArrayList<>();
+        drawCalls = new DrawCalls(mockCollection);
+    }
+
+    @Test
+    public void correctIfNoRecord() {
+        assertFalse(drawCalls.hasNextStep());
+        assertFalse(drawCalls.hasPrevStep());
+    }
+    
+    @Test
+    public void recordWorks() {
+        drawCalls.record();
+
+        assertTrue(drawCalls.hasNextStep());
+        assertNotNull(drawCalls.nextStep());
+    }
+
+    @Test
+    @Ignore
+    public void correctIfOneRecord() {
+        drawCalls.record();
+
+        assertTrue(drawCalls.hasNextStep());
+        assertFalse(drawCalls.hasPrevStep());
+    }
+}
