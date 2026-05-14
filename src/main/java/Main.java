fe5f53f6d02827621c20b927e4de87dc02be14f2
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-14 23:49:49 +0200

    first execution also gets thrown into the compiler

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import java.io.FileReader;
+import java.nio.file.Path;
+
+import com.hhu.util.FileManager;
 import com.hhu.util.Visualizer;
+import com.hhu.util.compiler.Compiler;
 
 public class Main {
     public static void main(String[] args) {
-        GraphvizApp graphvizApp = new GraphvizApp();
-        graphvizApp.build();
+        String clazz = FileManager.fileToString(Path.of("src/main/resources/GraphvizAppDefault.txt"));
+        Compiler.compile(clazz);
         Visualizer.show();
     }
 }
