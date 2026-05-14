2cf89d35fc26c10a75dfb85119b234b94a959929
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-14 23:49:26 +0200

    remove .record() calls

--- src/main/java/GraphvizApp.java
+++ src/main/java/GraphvizApp.java
@@ -8,17 +8,9 @@ public class GraphvizApp {
     public void build() {
         Stack<String> stack = new Stack<>();
         Visualizer.register(stack);
-
         stack.push("Hello");
-        Visualizer.record();
-
         stack.push("Progra");
-        Visualizer.record();
-
         stack.push("!");
-        Visualizer.record();
-
         stack.pop();
-        Visualizer.record();
     }
 }
