70f880d77e0ad98bc445a78eff545c97a8df618d
Alexa <147258577+alexabrock@users.noreply.github.com>
2026-05-31 21:50:32 +0200

    edit colors

--- src/main/java/com/hhu/ui/Colors.java
+++ src/main/java/com/hhu/ui/Colors.java
@@ -5,17 +5,17 @@ import java.awt.Color;
 public class Colors {
     // Memory Colors
     public static final String BG_COLORM = "#ffffff";
-    public static final String FG_COLORM = "#1A1A1A";
+    public static final String FG_COLORM = "#002867"; //Font
     public static final String EDGE_COLORM = "#7F7F7F";
-    public static final String NODE_FILL_COLORM = "#fff0bf"; // FFF9E6
-    public static final String NODE_BORDER_COLORM = "#fff0bf"; // eabb00
+    public static final String NODE_FILL_COLORM = "#ffeec4";
+    public static final String NODE_BORDER_COLORM = "#ffeec4"; // ffc83d
 
     // Datastructure Colors
     public static final String BG_COLORD = "#ffffff";
-    public static final String FG_COLORD = "#1A1A1A";
+    public static final String FG_COLORD = "#1e293b"; // Font
     public static final String EDGE_COLORD = "#7F7F7F";
-    public static final String NODE_FILL_COLORD = "#CBE7F5";
-    public static final String NODE_BORDER_COLORD = "#0077B3";
+    public static final String NODE_FILL_COLORD = "#b5e8ff";
+    public static final String NODE_BORDER_COLORD = "#00b0ff";
 
     // code colors
     public static final Color LINE_HIGHLIGHT = new Color(250, 220, 233);
@@ -29,7 +29,7 @@ public class Colors {
     // application colors
     public static final Color APPLICATION_BACKGROUND = Color.WHITE;
     public static final Color CODE_PANEL_BACKGROUND = new Color(242, 242, 242);
-    public static final Color FONT = new Color(81, 81, 89);
+    public static final Color FONT = new Color(30, 41, 59);
 
     private Colors() {
     }
